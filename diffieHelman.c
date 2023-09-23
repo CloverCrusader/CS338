990bffeed644ee54aa6ef3da278f2fda6447507f
CloverCrusader <80260043+CloverCrusader@users.noreply.github.com>
2023-09-23 16:12:36 -0500

    Add files via upload
    uploaded Eve's Diffie-Helman solver.

---
+++ diffieHelman.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int p;
+int q;
+int remA;
+int remB;
+
+/* brute forces the power to which p was raised
+checking the if result mod q equals the remainder.
+returns suitable exponent.
+WARNING will continue indefinitely
+until a suitable exponent is found! */
+int findExp(int remainder) {
+    int exp = 0;
+    int num;
+    int next;
+    while (exp == 0) {
+        num++;
+        next = p^num % q;
+        if (next == remainder) {
+            exp = num;
+            break;
+        }
+    }
+    return exp;
+}
+
+/* prompts user for variable input for use in calculation */
+int main(int argc, char *args[]) {
+    printf("enter p, q, A, B\n");
+    scanf("%i %i %i %i", &p, &q, &remA, &remB);
+    int a = findExp(remA);
+    int b = findExp(remB);
+    printf("a: %i  b: %i\n", a, b);
+    return 0;
+}
